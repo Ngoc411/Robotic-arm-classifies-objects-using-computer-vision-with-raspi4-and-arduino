5acaf81b381cfc274a5b11023a02843fffdb911c
Ngoc411 <lxn4112004@gmail.com>
2025-01-26 02:30:31 +0700

    Update detect_line.py

--- detect_line.py
+++ detect_line.py
@@ -10,28 +10,23 @@ def detect_line(frame):
     max_len_line = 0
     max_len_line_index = None
 
-    # Convert to grayscale image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Use Canny to find edges
     edges = cv2.Canny(gray, 50, 200, None, 3)
 
-    # Detect lines using Probabilistic Hough Transform
     linesP = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, None, 60, 60)
 
     if linesP is not None:
         for i, lineP in enumerate(linesP):
-            # Calculate the length of each detected line
+
             len_line = math.sqrt((lineP[0][2] - lineP[0][0])**2 + (lineP[0][3] - lineP[0][1])**2)
 
             if len_line > max_len_line:
                 max_len_line = len_line
                 max_len_line_index = i
 
-            # Draw each detected line on the frame
             cv2.line(frame, (lineP[0][0], lineP[0][1]), (lineP[0][2], lineP[0][3]), (0, 0, 255), 3, cv2.LINE_AA)
 
-        # If a longest line is found, calculate its angle
         if max_len_line != 0 and max_len_line_index is not None:
             degree_theta = math.degrees(
                 math.atan2(
@@ -47,23 +42,20 @@ def detect_line(frame):
 
             print(f"Longest Line Angle: {degree_theta}°")
 
-# Example usage of the detect_line function
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(1)  # Open the default camera | can swtich it to 0,2, ...
-    cap.set(3, 640)  # Set frame width
-    cap.set(4, 480)  # Set frame height
+    cap = cv2.VideoCapture(1)  # Can swtich it to 0,2, ...
+    cap.set(3, 640)  
+    cap.set(4, 480)  
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        detect_line(frame)  # Detect and annotate lines in the frame
+        detect_line(frame) 
 
-        # Show the frame with detected lines
         cv2.imshow('Line Detection', frame)
 
-        # Exit the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
